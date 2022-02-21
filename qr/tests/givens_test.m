5087880305a794308d9f444a29bcb18c52b703e0
Iulian277 <iulian27_marius@yahoo.com>
2022-02-21 18:44:48 +0200

    added qr_givens

---
+++ qr/tests/givens_test.m
@@ -0,0 +1,20 @@
+% [ABOUT]
+%       This is a tester function for QR decomposition,
+%       using Givens rotation
+% [USES]
+%       qr/givens.m 
+
+function [] = givens_test()
+  n = 3;
+  A = rand(n, n);
+  b = rand(n, 1);
+
+  [Q R x_alg] = givens_(A, b);
+  
+  x_alg;
+  x_true  = A \ b;
+  err_sol = norm(x_true - x_alg)
+  
+  A_recomp = Q * R;
+  err_mat  = norm(A - A_recomp)
+endfunction
