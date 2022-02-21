b7be95e0945322332c75f2b3a2ec1cc7f25766e1
Iulian277 <iulian27_marius@yahoo.com>
2022-02-21 17:59:38 +0200

    added textbook

---
+++ qr/givens.m
@@ -0,0 +1,11 @@
+% [ABOUT]
+%       This function is used for Givens decomposition (A = Q * R)
+%       After that, solves theupper triangular system
+%       Returns the Q, R matrices and the solution of the initial system
+% [USES] 
+%       utils/SST.m
+
+function [Q R x] = givens(A, b)
+  
+  
+endfunction
