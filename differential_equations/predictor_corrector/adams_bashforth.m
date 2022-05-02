1629c9713c104dc601a7c5e64914553d459ec1a5
Iulian Taiatu <31247431+Iulian277@users.noreply.github.com>
2022-05-02 23:01:17 +0300

    Update adams_bashforth.m

--- differential_equations/predictor_corrector/adams_bashforth.m
+++ differential_equations/predictor_corrector/adams_bashforth.m
@@ -1,1 +1,18 @@
-% [ABOUT]%       This function is used for solving an ODE of first order,%       using the `Explicit Adams-Bashforth` method of 3rd order.%       Adams-Bashforth's method creats a interpolation polynomial%       between those 3 points and approximates `y_next` (which is outside%       the interpolation range). Therefore, it makes an `extrapolation`%       based on the last 3 given values (it "anticipates" the `y_next` value).% [INPUT]%       `y_curr`  - Current approximation%       `h`       - Step  %       `f_n`, `f_n_1`, `f_n_2`%                 - Values of function `f` in the last 3 points% [OUTPUT]%       `y_next`  - `Predicted` value of function `y` at next stepfunction [y_next] = adams_bashforth(y_curr, h, f_n, f_n_1, f_n_2)  y_next = y_curr + h * (23/12 * f_n - 4/3 * f_n_1 + 5/12 * f_n_2);endfunction
+% [ABOUT]
+%       This function is used for solving an ODE of first order,
+%       using the `Explicit Adams-Bashforth` method of 3rd order.
+%       Adams-Bashforth's method creats a interpolation polynomial
+%       between those 3 points and approximates `y_next` (which is outside
+%       the interpolation range). Therefore, it makes an `extrapolation`
+%       based on the last 3 given values (it "anticipates" the `y_next` value).
+% [INPUT]
+%       `y_curr`  - Current approximation
+%       `h`       - Step
+%       `f_n`, `f_n_1`, `f_n_2`
+%                 - Values of function `f` in the last 3 points
+% [OUTPUT]
+%       `y_next`  - `Predicted` value of function `y` at next step
+
+function [y_next] = adams_bashforth(y_curr, h, f_n, f_n_1, f_n_2)
+  y_next = y_curr + h * (23/12 * f_n - 4/3 * f_n_1 + 5/12 * f_n_2);
+endfunction
