462fb404b0a1efb657f97727ade78171a164907d
Iulian <iulian27_marius@yahoo.com>
2023-04-23 10:23:13 +0300

    swap some login in the `bisection_method.m` alg

--- nonlinear_equations/bisection_method.m
+++ nonlinear_equations/bisection_method.m
@@ -6,45 +6,42 @@
 %       This algorithm is similar to binary search
 
 function [x step] = bisection_method(f, a, b, tol, max_iter)
-    % Sanity checks
+    % Sanity check for interval
     if a > b
-        disp('Given interval is not correct!');
+        disp('Provided interval is not correct!');
         x = NaN;
         step = -1;
         return;
     endif
-    
+
+    % Sanity check for root in the interval
     if feval(f, a) * feval(f, b) > 0
         disp('No roots in the given interval!');
         x = NaN;
         step = -1;
         return;
     endif
-    
+
     % Root finding
     for step = 1 : max_iter
         % Compute the midpoint: (a + b) / 2
         x = a + (b - a) / 2;
-        
+
         % Compute the product f(left) * f(mid)
         f_prod = feval(f, a) * feval(f, x);
-        
-        % Check if we go to the left/right subinterval
-        % Or found the root
-        if f_prod < 0
-            b = x;
-        elseif f_prod > 0
-            a = x;
-        else
-            return;
-        endif
-        
-        % Check if we found the root
-        % Or the bisection method reached convergence limit
+
+        % Check if the bisection method reached
+        % convergence limit (found the root)
         if abs(feval(f, x)) < tol && abs(b - a) < tol
             return;
         endif
-        
+
+        % Check if we go to the left/right subinterval
+        if f_prod < 0
+            b = x;
+        else
+            a = x;
+        endif
     endfor
-    
+
 endfunction
--- nonlinear_equations/tests/bisection_method_test.m
+++ nonlinear_equations/tests/bisection_method_test.m
@@ -1,5 +1,5 @@
 % [ABOUT]
-%       This is a tester function for solving a 
+%       This is a tester function for solving a
 %       non-linear equation f(x) = 0, using the Bisection method
 % [USES]
 %       nonlinear_equations/bisection_method.m
@@ -7,25 +7,25 @@
 function [] = bisection_method_test()
     % Input function
     f = @(x) x.^2 - 5;
-    
+
     % Interval limits
     a = 2;
     b = 3;
     % Tolerance
     tol = 1e-5;
     % Maximum number of iterations
-    max_iter = 1e3;    
-    
+    max_iter = 1e3;
+
     [x_alg steps] = bisection_method(f, a, b, tol, max_iter)
-    
+
     x_true = fsolve(f, (a + b) / 2)
     err_sol = norm(x_true - x_alg)
-    
+
     % x = linspace(-10, 10, 100);
     % plot(x, feval(f, x));
     % hold on;
     % plot([-10 10], [0 0]); % Ox
     % plot([x_true x_true], [-20 100]); % x_true
     % hold off;
-    
+
 endfunction
